378d0b3673883fa9be9a98223d6ac75a63394328
alex <alex28042@gmail.com>
2020-11-18 20:23:10 +0100

    falta .gitignore y falta hacer listado de de las urls

---
+++ main.py
@@ -0,0 +1,31 @@
+import enum
+from typing import Coroutine
+from fastapi import FastAPI
+from starlette.responses import RedirectResponse
+from enum import Enum
+from pydantic import BaseModel
+
+
+
+app = FastAPI()
+
+class url_names(str, Enum):
+    core = "core"
+
+@app.get("/")
+async def root():
+    return {"message": "Url-shortener"}
+
+@app.get("/url-list")
+async def url_list(list: url_names):
+    return list
+
+@app.get("/url-list/{url}")
+async def get_url(url: url_names):
+    if url.value == "core":
+        return {"message":"https://github.com/orgs/CoreDumped-ETSISI/teams/academy-2020-web-engineering"}
+
+@app.get("/redirect")
+async def redirect():
+    return RedirectResponse('/')
+
