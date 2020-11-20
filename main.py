35ba0269f027cf4d41f45cc89b4689b33bacf0ec
alex <alex28042@gmail.com>
2020-11-20 11:32:37 +0100

    gitignore

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
-import enum
-from typing import Coroutine
 from fastapi import FastAPI
 from starlette.responses import RedirectResponse
 from enum import Enum
-
-
+from pydantic import BaseModel
 
 app = FastAPI()
 
-class url_names(str, Enum):
-    core = "core"
+class url_add(BaseModel):
+    url: str
+    url_short: str
 
 @app.get("/")
 async def root():
     return {"message": "Url-shortener"}
 
-@app.get("/url-list")
-async def url_list(list: url_names):
-    return list
+@app.get("/urls")
+async def url_list(urls: url_add):
+    return urls
 
-@app.get("/url-list/{url}")
-async def get_url(url: url_names):
-    if url.value == "core":
-        return {"message":"https://github.com/orgs/CoreDumped-ETSISI/teams/academy-2020-web-engineering"}
+@app.post("/url-list")
+async def add_url(urls: url_add):
+    return urls
+
+@app.get("/url-list/{urls}")
+async def get_url(urls: url_add):
+    if urls.value == "core":
+        return RedirectResponse(url = 'https://github.com/orgs/CoreDumped-ETSISI/teams/academy-2020-web-engineering')
 
 @app.get("/redirect")
 async def redirect():
