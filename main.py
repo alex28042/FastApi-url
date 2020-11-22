997e9c6d9a46917dd0d57ef3d4bcdf001d3a9abe
alex <alex28042@gmail.com>
2020-11-22 16:56:45 +0100

    arreglando

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from starlette.responses import RedirectResponse
-from enum import Enum
 from pydantic import BaseModel
+
+
 
 app = FastAPI()
 
@@ -9,22 +10,26 @@ class url_add(BaseModel):
     url: str
     url_short: str
 
+dic = []
+
 @app.get("/")
 async def root():
     return {"message": "Url-shortener"}
 
 @app.get("/urls")
-async def url_list(urls: url_add):
-    return urls
+async def url_list():
+    return dic
+
+@app.get("/urls/{url_item}")
+async def get_url(url: str):
+    return dic[url - 1]
+
 
 @app.post("/url-list")
 async def add_url(urls: url_add):
-    return urls
+    dic.append(urls)
+    return dic[- 1]
 
-@app.get("/url-list/{urls}")
-async def get_url(urls: url_add):
-    if urls.value == "core":
-        return RedirectResponse(url = 'https://github.com/orgs/CoreDumped-ETSISI/teams/academy-2020-web-engineering')
 
 @app.get("/redirect")
 async def redirect():
