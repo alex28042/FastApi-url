ffccb2a2b5fe93d490db744dccc9e6b103ebd8e7
alex <alex28042@gmail.com>
2020-12-04 11:54:42 +0100

    implementando auteticacion

--- main.py
+++ main.py
@@ -1,23 +1,39 @@
-from fastapi import FastAPI
+from typing import Optional
+from fastapi import Depends, FastAPI
 from starlette.responses import RedirectResponse
 from pydantic import BaseModel
-
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 
 app = FastAPI()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl = "token")
 
 class url_add(BaseModel):
     url: str
     url_short: str
 
+class User(BaseModel):
+    username: str
+    email: Optional[str] = None
+    full_name: Optional[str] = None
+    disabled: Optional[bool] = None
+
 dic = []
+fake_users_db = {}
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    return user
+
+def fake_decode_token(token):
+    return User(username = token + "fakedecoded", email = "john@example.com", full_name = "Alex")
 
 @app.get("/")
 async def root():
     return {"message": "Url-shortener"}
 
 @app.get("/urls")
-async def url_list():
+async def url_list(token: str = Depends(oauth2_scheme)):
     return dic
 
 @app.get("/{url_short}")
